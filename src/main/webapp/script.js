1d39432c931551d3730258e5e94b93a8289203fc
AlejandroPortilla <124326061+AlejandroPortilla@users.noreply.github.com>
2023-10-25 09:24:54 -0500

    documentando script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,13 +1,18 @@
-const container = document.getElementById('container');
-                        const registerBtn = document.getElementById('register');
-                        const loginBtn = document.getElementById('login');
+// Obtiene los elementos HTML por su ID
+const container = document.getElementById('container'); // El div que contiene las pantallas de registro y login.
+const registerBtn = document.getElementById('register'); // El botón para mostrar la pantalla de registro.
+const loginBtn = document.getElementById('login'); // El botón para mostrar la pantalla de inicio de sesión.
 
-                        registerBtn.addEventListener('click', () => {
-                        container.classList.add("active");
-                        });
+// Agrega un event listener al botón de registro
+registerBtn.addEventListener('click', () => {
+  // Cuando se hace clic en el botón de registro, agrega la clase "active" al contenedor.
+  container.classList.add("active");
+});
 
-                        loginBtn.addEventListener('click', () => {
-                        container.classList.remove("active");
-            });
+// Agrega un event listener al botón de inicio de sesión
+loginBtn.addEventListener('click', () => {
+  // Cuando se hace clic en el botón de inicio de sesión, elimina la clase "active" del contenedor.
+  container.classList.remove("active");
+});
 
 
